1932fc0805c9cb6db0459df0ed75bddaf57b13f2
Dmitrii <my@dimka89.ru>
2024-04-19 22:33:13 +0300

    Developmen

---
+++ Development_of_a_network/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Development_of_a_network
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
